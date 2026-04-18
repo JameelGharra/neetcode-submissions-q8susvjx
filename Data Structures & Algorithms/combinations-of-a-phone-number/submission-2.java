61f0dfcf3e6372b3014addd8a1490ee11501ab33
JameelGharra <jameelgharra@gmail.com>
2026-04-18 19:53:11 +0300

    Add: combinations-of-a-phone-number - submission-2

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-2.java
@@ -0,0 +1,24 @@
+class Solution {
+    // iteratively
+    // O(i) for each level's node for work for concatenation
+    // this solution is kinda bad for garbage collected languages
+    public List<String> letterCombinations(String digits) {
+        if (digits.isEmpty()) return new ArrayList<>();
+        String[] letters = {
+            "", "", "abc", "def", "ghi", "jkl",
+            "mno", "qprs", "tuv", "wxyz"
+        };
+        List<String> res = new ArrayList<>();
+        res.add("");
+        for (char c : digits.toCharArray()) {
+            List<String> tmp = new ArrayList<>();
+            for (char letter : letters[c - '0'].toCharArray()) {
+                for (String combo : res) {
+                    tmp.add(combo + letter);
+                }
+            }
+            res = tmp;
+        }
+        return res;
+    }
+}
