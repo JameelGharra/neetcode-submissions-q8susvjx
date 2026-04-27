35141a4f02b44a8c29a6e91e10bb05adda837cd7
JameelGharra <jameelgharra@gmail.com>
2026-04-27 20:34:07 +0300

    Add: clone-graph - submission-4

---
+++ Data Structures & Algorithms/clone-graph/submission-4.java
@@ -0,0 +1,43 @@
+/*
+Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    public Node cloneGraph(Node node) {
+        if (node == null)
+            return null;
+
+        Map<Node, Node> oldToNew = new HashMap<>();
+        Queue<Node> queue = new ArrayDeque<>();
+        oldToNew.put(node, new Node(node.val));
+        queue.add(node);
+        while (!queue.isEmpty()) {
+            Node nodeToProcess = queue.poll();
+            List<Node> eqNodeNeighbors = oldToNew.get(nodeToProcess).neighbors;
+            for (Node neighbor : nodeToProcess.neighbors) {
+                if (!oldToNew.containsKey(neighbor)) {
+                    oldToNew.put(neighbor, new Node(neighbor.val));
+                    queue.add(neighbor);
+                }
+                eqNodeNeighbors.add(oldToNew.get(neighbor));
+            }
+        }
+        return oldToNew.get(node);
+    }
+}
