aa4c06797bed247aa85992c35952f816e56f3b23
JameelGharra <jameelgharra@gmail.com>
2026-05-05 17:11:00 +0300

    Add: valid-tree - submission-4

---
+++ Data Structures & Algorithms/valid-tree/submission-4.java
@@ -0,0 +1,46 @@
+class Solution {
+    // disjoint, whats good is that we dont need to build adjacency list
+    public boolean validTree(int n, int[][] edges) {
+        if (edges.length != n - 1)
+            return false;
+        UnionFind uf = new UnionFind(n);
+        for (int[] edge : edges) {
+            if (!uf.union(edge[0], edge[1]))
+                return false;
+        }
+        // return uf.size[parent[0]] == n;
+        return true;
+    }
+    private static class UnionFind {
+        int[] parent;
+        int[] size;
+        UnionFind(int n) {
+            parent = new int[n];
+            size = new int[n];
+            for (int i = 0; i < n; ++i) {
+                size[i] = 1;
+                parent[i] = i;
+            }
+        }
+        int find(int x) {
+            if (parent[x] == x)
+                return x;
+            parent[x] = find(parent[x]);
+            return parent[x];
+        }
+        boolean union(int x, int y) {
+            int rootX = find(x), rootY = find(y);
+            if (rootX == rootY)
+                return false;
+            if (size[rootX] >= size[rootY]) {
+                size[rootX]++;
+                parent[rootY] = rootX;
+            }
+            else {
+                size[rootY]++;
+                parent[rootX] = rootY;
+            }
+            return true;
+        }
+    }
+}
