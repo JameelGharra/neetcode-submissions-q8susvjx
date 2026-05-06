e28ca6f6c8c9c2dbb879b2e8183abf6d96978a00
JameelGharra <jameelgharra@gmail.com>
2026-05-06 21:59:59 +0300

    Add: redundant-connection - submission-0

---
+++ Data Structures & Algorithms/redundant-connection/submission-0.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int[] findRedundantConnection(int[][] edges) {
+        int n = 0;
+        for (int[] edge : edges) {
+            n = Math.max(n, Math.max(edge[0], edge[1]));
+        }
+        UnionFind uf = new UnionFind(n);
+        int[] res = null;
+        for (int[] edge : edges) {
+            if (!uf.union(edge[0], edge[1]))
+                res = edge;
+        }
+        return res;
+    }
+    private static class UnionFind {
+        int[] parent;
+        int[] rank;
+        UnionFind(int n) {
+            parent = new int[n + 1];
+            rank = new int[n + 1];
+            for (int i = 1; i <= n; ++i) {
+                parent[i] = i;
+                rank[i] = 1;
+            }
+        }
+        int find(int x) {
+            if (x == parent[x])
+                return x;
+            parent[x] = find(parent[x]);
+            return parent[x];
+        }
+        boolean union(int x, int y) {
+            int rootX = find(x), rootY = find(y);
+            if (rootX == rootY)
+                return false;
+            if (rank[rootX] == rank[rootY]) {
+                ++rank[rootX];
+            }
+            else if (rank[rootX] < rank[rootY]) {
+                int temp = rootX;
+                rootX = rootY;
+                rootY = temp;
+            }
+            parent[rootY] = rootX;
+            return true;
+        }
+    }
+}
