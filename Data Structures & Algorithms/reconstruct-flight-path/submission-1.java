ced72c04141a40ce15f6179b3c621778ac0e44d5
JameelGharra <jameelgharra@gmail.com>
2026-05-13 14:38:56 +0300

    Add: reconstruct-flight-path - submission-1

---
+++ Data Structures & Algorithms/reconstruct-flight-path/submission-1.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<String> findItinerary(List<List<String>> tickets) {
+        Map<String, PriorityQueue<String>> adjMap = new HashMap<>();
+        for (List<String> ticket : tickets) {
+            adjMap.computeIfAbsent(ticket.get(0), 
+                k -> new PriorityQueue<>()).offer(ticket.get(1));
+        }
+        LinkedList<String> itinerary = new LinkedList<>();
+        dfs("JFK", adjMap, itinerary);
+        return itinerary;
+    }
+
+    private void dfs(
+        String airport, 
+        Map<String, PriorityQueue<String>> adjMap, 
+        LinkedList<String> path
+    ) {
+        PriorityQueue<String> dests = adjMap.get(airport);
+        if (dests != null) {
+            while (!dests.isEmpty()) {
+                dfs(dests.poll(), adjMap, path);
+            }
+        }
+        path.addFirst(airport);    
+    }
+}
